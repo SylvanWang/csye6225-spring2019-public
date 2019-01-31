3b7ca22733b5f2f6569abee0efb346400725af86
yichuan95 <jiang.yic@husky.neu.edu>
2019-01-31 08:37:06 -0500

    email format validation

--- webapp/controller/user_controller.js
+++ webapp/controller/user_controller.js
@@ -13,18 +13,31 @@ function createUser(req, res) {
 
     var sql = 'INSERT INTO users(username,password) VALUES("' + username + '","' + password + '")';
 
-    DB.query(sql, function (err, result) {
-        if (err) {
-            console.log('[INSERT ERROR] - ', err.message);
-            return;
-        }
+    var emailCheck = validateEmail(username);
 
-        console.log('--------------------------INSERT----------------------------');
-        console.log(result);
-        res.status(200).send(result);
-        console.log('-----------------------------------------------------------------\n\n');
-    });
+
+    if(emailCheck) {
+        DB.query(sql, function (err, result) {
+            if (err) {
+                console.log('[INSERT ERROR] - ', err.message);
+                return;
+            }
+
+            console.log('--------------------------INSERT----------------------------');
+            console.log(result);
+            res.status(200).send(result);
+            console.log('-----------------------------------------------------------------\n\n');
+        });
+    }
+    else return res.status(400).send("Bad request: username is not an email");
+
 }
+
+function validateEmail(email) {
+    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+}
+
 
 function auth(req, res, next) {
 

3e8b7950a8e8fad15739e898c2a533b64a5d9c70
SylvanW <wang.mengqi2@husky.neu.edu>
2019-01-31 15:28:45 -0500

    seperate basic db functions from controller; forbid creating existing accounts;

--- webapp/controller/user_controller.js
+++ webapp/controller/user_controller.js
@@ -8,22 +8,35 @@ function getTime(req, res) {
 
 function createUser(req, res) {
 
+    console.log(req.body);
     var username = req.body.username;
     var password = req.body.password;
 
-    var sql = 'INSERT INTO users(username,password) VALUES("' + username + '","' + password + '")';
-
-    DB.query(sql, function (err, result) {
-        if (err) {
-            console.log('[INSERT ERROR] - ', err.message);
-            return;
+    var promise = DB.searchUser(username);
+    promise.then(function(value){
+        if(value){
+            console.log('search success!');
+            return res.status(400).send("User exists");
         }
-
-        console.log('--------------------------INSERT----------------------------');
-        console.log(result);
-        res.status(200).send(result);
-        console.log('-----------------------------------------------------------------\n\n');
+        else{
+            console.log('search fail');
+            var promise = DB.createUser(username,password);
+            promise.then(function(value){
+                if(value){
+                    console.log('insert success!');
+                    res.status(200).send(true);
+                }
+                else{
+                    console.log('insert fail!');
+                    res.status(200).send(false);
+                }
+            });
+        }
     });
+
+
+
+
 }
 
 function auth(req, res, next) {
@@ -41,18 +54,30 @@ function auth(req, res, next) {
             if (!user[1])
                 return unauthorized(res);
 
-        let sql = 'SELECT * FROM users WHERE username="' + user[0] + '"';
-        DB.query(sql, function (err, result) {
-            if (err) {
-                console.log('[INSERT ERROR] - ', err.message);
-                return;
-            }
-            if (!result[0])
-                return res.status(400).send("Invalid credentials");;
-            if (result[0].password === user[1])
+        var promise = DB.checkUser(user[0],user[1]);
+        promise.then(function(value){
+            if(value){
+                console.log('search success!');
                 next();
-            else return res.status(400).send("Invalid credentials");
+            }
+            else{
+                console.log('search fail');
+                return res.status(400).send("Invalid credentials");
+            }
         });
+
+        // let sql = 'SELECT * FROM users WHERE username="' + user[0] + '"';
+        // DB.query(sql, function (err, result) {
+        //     if (err) {
+        //         console.log('[INSERT ERROR] - ', err.message);
+        //         return;
+        //     }
+        //     if (!result[0])
+        //         return res.status(400).send("Invalid credentials");;
+        //     if (result[0].password === user[1])
+        //         next();
+        //     else return res.status(400).send("Invalid credentials");
+        // });
     } else {
         return unauthorized(res);
     }
--- webapp/routes/db.js
+++ webapp/routes/db.js
@@ -2,7 +2,7 @@ var mysql = require('mysql');
 var pool = mysql.createPool({
     host: 'localhost',
     user: 'root',
-    password: 'root',
+    password: 'rootroot',
     database: 'csye_6225'
 });
 
@@ -28,5 +28,70 @@ function query(sql, callback) {
         });
     });
 }
+function createUser(username,password){
+    var promise = new Promise(function(resolve){
+        var sql = 'INSERT INTO users(username,password) VALUES("' + username + '","' + password + '")';
+        pool.query(sql, function (err) {
+            if (err) {
+                console.log('[INSERT ERROR] - ', err.message);
+                return;
+            }
+            console.log('--------------------------INSERT----------------------------');
+            console.log(username + ' ' + password);
+            console.log('-----------------------------------------------------------------\n\n');
+            resolve(true);
+        });
+    });
+    return promise;
+}
 
-exports.query = query;
+function checkUser(username,password){
+    var promise = new Promise(function(resolve){
+        var sql = 'SELECT * FROM users WHERE username="' + username + '" AND password="' + password +'"';
+        pool.query(sql, function (err,result) {
+            if (err) {
+                console.log('[SEARCH ERROR] - ', err.message);
+                return;
+            }
+            console.log('--------------------------SEARCH----------------------------');
+            console.log(result);
+            console.log('-----------------------------------------------------------------\n\n');
+            if(result[0]){
+                resolve(true);
+            }
+            else{
+                resolve(false);
+            }
+        });
+    });
+    return promise;
+}
+
+function searchUser(username){
+    var promise = new Promise(function(resolve){
+        var sql = 'SELECT * FROM users WHERE username="' + username + '"';
+        pool.query(sql, function (err,result) {
+            if (err) {
+                console.log('[SEARCH ERROR] - ', err.message);
+                return;
+            }
+            console.log('--------------------------SEARCH----------------------------');
+            console.log(result);
+            console.log('-----------------------------------------------------------------\n\n');
+            if(result[0]){
+                resolve(true);
+            }
+            else{
+                resolve(false);
+            }
+        });
+    });
+    return promise;
+}
+
+module.exports = {
+    query,
+    createUser,
+    checkUser,
+    searchUser
+};

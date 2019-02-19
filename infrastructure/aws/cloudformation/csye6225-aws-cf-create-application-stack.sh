0cd332b1287960ef72332f09a42503924e7f44c6
SylvanW <wang.mengqi2@husky.neu.edu>
2019-02-18 20:49:42 -0500

    Create a new CloudFormation template csye6225-cf-application.json Create shell script csye6225-aws-cf-create-application-stack.sh Create shell script csye6225-aws-cf-terminate-application-stack.sh Update assignment2 scripts

---
+++ infrastructure/aws/cloudformation/csye6225-aws-cf-create-application-stack.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+vpcId=$(aws ec2  describe-vpcs --filters Name=tag:aws:cloudformation:logical-id,Values="myVPC" |jq '.Vpcs[0].VpcId')
+echo "$vpcId"
+publicSubnetId1=$(aws ec2 describe-subnets --filters Name=vpc-id,Values=$vpcId Name=tag:aws:cloudformation:logical-id,Values="myPublicSubnet1" |jq '.Subnets[0].SubnetId')
+echo $publicSubnetId1
+
+privateSubnetId1=$(aws ec2 describe-subnets --filters Name=vpc-id,Values=$vpcId Name=tag:aws:cloudformation:logical-id,Values="myPrivateSubnet1" |jq '.Subnets[0].SubnetId')
+echo $privateSubnetId1
+
+privateSubnetId2=$(aws ec2 describe-subnets --filters Name=vpc-id,Values=$vpcId Name=tag:aws:cloudformation:logical-id,Values="myPrivateSubnet2" |jq '.Subnets[0].SubnetId')
+echo $privateSubnetId2
+
+domainName=$(aws route53 list-hosted-zones | jq '.HostedZones[0].Name' | tr -d '"')
+echo $domainName
+
+if [ ! -z "$domainName" ]
+then
+      res=$(aws cloudformation create-stack --stack-name $1 --template-body \
+            file://csye6225-cf-application.json \
+            --parameters \
+                        ParameterKey=vpcId,ParameterValue=$vpcId \
+                        ParameterKey=publicSubnetId1,ParameterValue=$publicSubnetId1 \
+                        ParameterKey=privateSubnetId1,ParameterValue=$privateSubnetId1 \
+                        ParameterKey=privateSubnetId2,ParameterValue=$privateSubnetId2 \
+                        ParameterKey=DBport,ParameterValue="5432"\
+                        ParameterKey=DomainName,ParameterValue=$domainName;
+            aws cloudformation wait stack-create-complete --stack-name $1)
+
+else
+      echo "Domain is not yet hosted."
+fi
+
+echo $res
+if [ ! -z "$res" ]
+then
+      echo "\$success"
+
+else
+      echo "\$fail"
+fi

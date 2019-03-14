2b8fbd55bd42541a66a260dd67cff89b9487b699
Ranran He <he.ra@northeastern.edu>
2019-03-14 04:21:41 -0700

    test20

--- afterInstall.sh
+++ afterInstall.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
-pkill node
+sudo pkill node
 sudo chmod 777 -R /webapp
-sudo systemctl start nodeserver.service && journalctl -fexu nodeserver.service
+#sudo systemctl start nodeserver.service && journalctl -fexu nodeserver.service
+sudo systemctl status --no-pager nodeserver.service
 cd webapp
 npm install
-mysql -u csye6225master -pcsye6225password -e "use csye6225;
-create table if not exists users(id int(11) not null auto_increment, username VARCHAR(45), password VARCHAR(100), constraint pk_example primary key (id));
-create table if not exists notes(id VARCHAR(100) not null, content VARCHAR(45), title VARCHAR(45), createdOn VARCHAR(45), lastUpdatedOn VARCHAR(45), creator_id VARCHAR(45) not null, constraint pk_example primary key (id));
-create table if not exists attachments(id VARCHAR(100) not null, url VARCHAR(245), _key VARCHAR(245), noteId VARCHAR(100) not null);"
-
 node ./bin/www

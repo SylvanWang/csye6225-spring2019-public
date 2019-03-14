5b9af2ef2f621a6f8b86edb2ffdab39096931a03
Ranran He <he.ra@northeastern.edu>
2019-03-14 01:23:24 -0700

    test16

--- afterInstall.sh
+++ afterInstall.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 pkill node
-sudo chmod 777 -R /opt/webapp
-sudo systemctl start nodeserver.service
-cd /opt/webapp
+sudo chmod 777 -R /webapp
+sudo systemctl start nodeserver.service && journalctl -fexu nodeserver.service
+cd webapp
 npm install
 node ./bin/www

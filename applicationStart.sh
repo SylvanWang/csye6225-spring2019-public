519374345935cddcbbe767718997dc5425ffaca2
Ranran He <he.ra@northeastern.edu>
2019-03-14 16:02:20 -0700

    test2

--- applicationStart.sh
+++ applicationStart.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
+sudo systemctl enable nodeserver.service
 sudo systemctl start nodeserver.service && journalctl -fexu nodeserver.service >run.out 2>run.err &
+sudo systemctl status nodeserver.service -l

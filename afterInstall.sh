0e61509457f13262a65e8583e8df3c77e0289724
SylvanWang <15801592228@163.com>
2019-03-14 19:37:05 -0400

    Update afterInstall.sh

--- afterInstall.sh
+++ afterInstall.sh
@@ -5,8 +5,8 @@ sudo chmod 777 -R /webapp
 cd webapp
 npm install
 
-mysql -h csye6225-spring2019.c4dnt3p8gjkt.us-east-1.rds.amazonaws.com -P 3306 -u csye6225master -pcsye6225password -e 'use csye6225; create table if not exists users(id int(11) not null auto_increment, username VARCHAR(45), password VARCHAR(100), constraint pk_example primary key (id));'
-mysql -h csye6225-spring2019.c4dnt3p8gjkt.us-east-1.rds.amazonaws.com -P 3306 -u csye6225master -pcsye6225password -e 'use csye6225; create table if not exists notes(id VARCHAR(100) not null, content VARCHAR(45), title VARCHAR(45), createdOn VARCHAR(45), lastUpdatedOn VARCHAR(45), creator_id VARCHAR(45) not null, constraint pk_example primary key (id));'
-mysql -h csye6225-spring2019.c4dnt3p8gjkt.us-east-1.rds.amazonaws.com -P 3306 -u csye6225master -pcsye6225password -e 'use csye6225; create table if not exists attachments(id VARCHAR(100) not null, url VARCHAR(245), _key VARCHAR(245), noteId VARCHAR(100) not null);'
+mysql -h csye6225-spring2019.cvbzr0rb8azs.us-east-1.rds.amazonaws.com -P 3306 -u csye6225master -pcsye6225password -e 'use csye6225; create table if not exists users(id int(11) not null auto_increment, username VARCHAR(45), password VARCHAR(100), constraint pk_example primary key (id));'
+mysql -h csye6225-spring2019.cvbzr0rb8azs.us-east-1.rds.amazonaws.com -P 3306 -u csye6225master -pcsye6225password -e 'use csye6225; create table if not exists notes(id VARCHAR(100) not null, content VARCHAR(45), title VARCHAR(45), createdOn VARCHAR(45), lastUpdatedOn VARCHAR(45), creator_id VARCHAR(45) not null, constraint pk_example primary key (id));'
+mysql -h csye6225-spring2019.cvbzr0rb8azs.us-east-1.rds.amazonaws.com -P 3306 -u csye6225master -pcsye6225password -e 'use csye6225; create table if not exists attachments(id VARCHAR(100) not null, url VARCHAR(245), _key VARCHAR(245), noteId VARCHAR(100) not null);'
 
 #node ./bin/www

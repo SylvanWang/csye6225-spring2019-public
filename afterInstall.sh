9b0ac2d8d0c3fb2e3c76e90fbe130021c0ab8547
Ranran He <he.ra@northeastern.edu>
2019-03-13 23:47:29 -0700

    test9

--- afterInstall.sh
+++ afterInstall.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 pkill node
 sudo chmod 777 -R webapp
+sh /etc/profile.d/webappEnv.sh
+sudo systemctl start /etc/systemd/system/nodeserver.service
 cd /webapp
 npm install
 sudo systemctl start nodeserver.service

a3ded753c54898790569ed0233dadc5dee2ee285
Ranran He <he.ra@northeastern.edu>
2019-03-14 00:47:26 -0700

    test15

--- afterInstall.sh
+++ afterInstall.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 pkill node
-sudo chmod 777 -R webapp
-sudo systemctl daemon-reload
-sudo systemctl enable nodeserver.service
+sudo chmod 777 -R /opt/webapp
 sudo systemctl start nodeserver.service
-cd /webapp
+cd /opt/webapp
 npm install
 node ./bin/www

868e185f5ef1e4548807fe5c8844163c4ca64a0e
Ranran He <he.ra@northeastern.edu>
2019-03-14 18:35:43 -0700

    test

--- afterInstall.sh
+++ afterInstall.sh
@@ -4,4 +4,6 @@ sudo chmod 777 -R /webapp
 #sudo systemctl start nodeserver.service && journalctl -fexu nodeserver.service
 cd webapp
 npm install
+sudo systemctl status nodeserver.service -l
+sudo systemctl start nodeserver.service && journalctl -fexu nodeserver.service >run.out 2>run.err &
 #node ./bin/www
--- applicationStart.sh
+++ applicationStart.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-sudo systemctl status nodeserver.service -l
 sudo systemctl start nodeserver.service && journalctl -fexu nodeserver.service >run.out 2>run.err &
 

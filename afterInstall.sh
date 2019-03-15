efec4bc9fb62bc344778402c32ea5d3ad0b828b0
Ranran He <he.ra@northeastern.edu>
2019-03-14 18:39:09 -0700

    test1

--- afterInstall.sh
+++ afterInstall.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
-sudo pkill node
+
 sudo chmod 777 -R /webapp
+sudo pkill node
 #sudo systemctl start nodeserver.service && journalctl -fexu nodeserver.service
 cd webapp
 npm install
+node ./bin/www >www.out 2>www.err &
+sudo pkill node
 sudo systemctl status nodeserver.service -l
 sudo systemctl start nodeserver.service && journalctl -fexu nodeserver.service >run.out 2>run.err &
 #node ./bin/www

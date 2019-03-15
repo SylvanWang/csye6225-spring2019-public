5daa0b7977f4486cff97c84b9e81825bfa541393
Ranran He <he.ra@northeastern.edu>
2019-03-14 17:00:28 -0700

    test upload

--- webapp/service/aws_service.js
+++ webapp/service/aws_service.js
@@ -3,32 +3,33 @@ const s3 = new AWS.S3();
 const { S3BUCKET } = require('../config')[process.env.NODE_ENV];
 const fs = require('fs');
 
-console.log("S3BUCKET: " + S3BUCKET);
-
 getFileData = (files) => {
     console.log(files);
     return new Promise((res, rej) => {
         let fileData;
         if (process.env.NODE_ENV === 'dev') {
+            s3.listBuckets(function(err, data) {
+                if (err) {
+                    console.log("Error", err);
+                } else {
 
-            let promiseArray = files.map(f => {
-                let params = {Bucket: S3BUCKET, Key: `${Date.now()}-${f.originalname}`, Body: f.buffer};
-                return s3.upload(params).promise();
+                    let promiseArray = files.map(f => {
+                        let params = { Bucket:S3BUCKET, Key:`${Date.now()}-${f.originalname}`, Body: f.buffer };
+                        console.log(params);
+                        return s3.upload(params).promise();
+                    });
+
+                    Promise.all(promiseArray).then(result => {
+                        fileData = result.map(file => { return {"location":file.Location, "key":file.Key}});
+                        console.log('S3 data ->', fileData);
+                        res(fileData);
+                    }).catch(err=>
+                        rej(err)
+                    );
+                }
             });
-
-            Promise.all(promiseArray).then(result => {
-                fileData = result.map(file => {
-                    return {"location": file.Location, "key": file.Key}
-                });
-                console.log('S3 data ->', fileData);
-                res(fileData);
-            }).catch(err =>
-                rej(err)
-            );
         } else if (process.env.NODE_ENV === 'default') {
-            fileData = files.map(file => {
-                return {"location": file.path, "key": file.filename}
-            });
+            fileData = files.map(file => { return {"location":file.path, "key":file.filename}});
             console.log('Local data ->', fileData);
             res(fileData);
         }
@@ -41,22 +42,31 @@ updateFile = (data, file) => {
 
         if (process.env.NODE_ENV === 'dev') {
 
-            let params = {
-                Bucket: S3BUCKET,
-                Key: `${Date.now()}-${file.originalname}`,
-                Body: file.buffer
-            };
-            let result = s3.upload(params).promise();
+            s3.listBuckets(function(err, bucket) {
+                if (err) {
+                    console.log("Error", err);
+                } else {
+                    console.log("000000000")
+                    console.log(file)
+                    let params1 = {Bucket: S3BUCKET, Key: data[0]._key};
+                    let params = {Bucket: S3BUCKET, Key: `${Date.now()}-${file.originalname}`, Body: file.buffer};
 
-            result.then(result => {
-                let resultData = {"location": result.Location, "key": result.Key};
-                res(resultData);
-            }).catch(err =>
-                rej(err)
-            )
+                    let result1 = s3.deleteObject(params1).promise();
+                    let result = s3.upload(params).promise();
+
+                    result.then(result => {
+                        let resultData = {"location": result.Location, "key": result.Key};
+                        console.log('Filedata', resultData)
+                        res(resultData);
+                    }).catch(err =>
+                        rej(err)
+                    )
+                }
+            });
+
         } else if (process.env.NODE_ENV === 'default') {
             deleteFileS3(data[0]._key).then(() => {
-                fileData = {"location": file.path, key: `${Date.now()}-${file.originalname}`};
+                fileData =  {"location":file.path, key: `${Date.now()}-${file.originalname}`};
                 console.log('Filedata', fileData);
                 res(fileData);
             })
@@ -67,12 +77,16 @@ updateFile = (data, file) => {
 deleteFileS3 = (key) => {
     return new Promise((res, rej) => {
         if (process.env.NODE_ENV === 'dev') {
-
-            let params = {Bucket: S3BUCKET, Key: key};
-            let result = s3.deleteObject(params).promise();
-
-            result.then(result => res()).catch(err => rej(err));
-
+            s3.listBuckets(function(err, data) {
+                if (err) {
+                    console.log("Error", err);
+                } else {
+                    let params = {Bucket: S3BUCKET, Key: key};
+                    let result = s3.deleteObject(params).promise();
+data.Buckets[0].Name
+                    result.then(result => res()).catch(err => rej(err));
+                }
+            });
 
         } else if (process.env.NODE_ENV === 'default') {
             if (fs.existsSync(`uploads/${key}`)) {

d19589475f1288e760de061c44ccdae84b42309c
Ranran He <he.ra@northeastern.edu>
2019-03-15 15:03:22 -0700

    test2

--- afterInstall.sh
+++ afterInstall.sh
@@ -3,10 +3,5 @@ source /home/centos/.bash_profile
 sudo chmod 777 -R /webapp
 sudo pkill node
 sudo systemctl daemon-reload
-#sudo systemctl start nodeserver.service && journalctl -fexu nodeserver.service
 cd webapp
 npm install
-#node ./bin/www >www.out 2>www.err &
-#sudo systemctl status nodeserver.service -l
-#sudo systemctl start nodeserver.service && journalctl -fexu nodeserver.service >run.out 2>run.err &
-#node ./bin/www
--- applicationStart.sh
+++ applicationStart.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 source /home/centos/.bash_profile
 npm install -g forever
-#sudo systemctl start nodeserver.service && journalctl -fexu nodeserver.service >run.out 2>run.err &
-forever node /webapp/bin/www &
+nohup node /webapp/bin/www &

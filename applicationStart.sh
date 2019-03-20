655a2ce12ff3ef8ccef22dae8a95d357a577765a
Ranran He <he.ra@northeastern.edu>
2019-03-20 16:43:27 -0700

    fix

--- applicationStart.sh
+++ applicationStart.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 source /home/centos/.bash_profile
-nohup node /webapp/bin/www >/dev/null 2>&1 &
+nohup node /webapp/bin/www > webapp.log 2> webapp_err.log &
+
+sudo systemctl enable awslogs.service
+sudo systemctl start awslogs.service

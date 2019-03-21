35305e1296a43ed711ec4b690dd7a52ad4865e08
Ranran He <he.ra@northeastern.edu>
2019-03-20 20:09:01 -0700

    test

--- applicationStart.sh
+++ applicationStart.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 source /home/centos/.bash_profile
+sudo systemctl start cloudwatch.service
+
 cd /webapp/node_logs
 nohup node /webapp/bin/www > webapp.log 2> webapp_err.log &
 
-sudo systemctl start cloudwatch.service

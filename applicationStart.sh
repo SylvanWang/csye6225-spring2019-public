e0822bffaf09e6fd3a8b22c9f74017a8270061b0
Ranran He <he.ra@northeastern.edu>
2019-03-20 21:19:32 -0700

    change metrics agg interval

--- applicationStart.sh
+++ applicationStart.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 source /home/centos/.bash_profile
-sudo systemctl start cloudwatch.service
 
 cd /webapp
 mkdir node_logs
 cd node_logs
 nohup node /webapp/bin/www > webapp.log 2> webapp_err.log &
+
+sudo systemctl start cloudwatch.service
 
